4642eeaaabfe1b1d17f46ebad7155c0bbe66dbf8
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-10-14 22:12:52 +0900

    finished

--- mvvm/ContentView.swift
+++ mvvm/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+//  models
+struct Person {
+    var name:String
+    var birthday:Date
+}
+
+//  viewmodel (logic)
+class ContentViewModel:ObservableObject {
+    //  noticicate change to model
+    @Published private var alice = Person(name: "Alice", birthday: Date())
+    
+    var name: String {
+        alice.name
+    }
+    
+    var age: String {
+        //  date magic -> age string
+        return "32"
+    }
+    
+    //  intent
+    func changeName(_ name:String) {
+        alice.name = name
+    }
+}
+
+//  views
 struct ContentView: View {
+    
+    @StateObject var viewModel = ContentViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text(viewModel.name)
+                .padding()
+            Text(viewModel.age)
+                .padding()
+            Button("change name") {
+                // change name to BOB
+                viewModel.changeName("BOB")
+            }
+        }.font(.largeTitle)
     }
 }
 
