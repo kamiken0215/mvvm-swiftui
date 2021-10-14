6f4273a4733828b3e8ae116f3fe30be4f12acd2e
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-10-14 21:48:53 +0900

    Initial Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by 神山賢太郎 on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
